205c943e958f6008ec3d79af815a392df84af6a4
Cadu3011 <cadulinker132@gmail.com>
2025-11-21 11:32:52 -0300

    feat(trier): adicionando devoluções

--- web/src/app/gerencia-cofre/gerencia-cartao/components/FormLoginTrier.tsx
+++ web/src/app/gerencia-cofre/gerencia-cartao/components/FormLoginTrier.tsx
@@ -21,6 +21,7 @@ export function LoginFormTrier({ token }: Props) {
     setLoad(true);
     //pegar controller fazendo get na pagina login , e efetuar login pegando o token e setando nos cookies
     const response = await LoginTrier(formData);
+    console.log(response?.data);
     if (response?.data) {
       setResponse(response.data);
       const showToast = () => {
